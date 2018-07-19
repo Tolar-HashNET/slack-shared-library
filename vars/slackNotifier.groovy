6d145465136c7d46cc4b098b5dea25642d8131a7
abhisheks-cuelogic <abhishek.soni@cuelogic.com>
2018-07-19 20:21:13 +0530

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -11,7 +11,7 @@ def call(String buildStatus = 'STARTED') {
   def color = 'RED'
   def colorCode = '#FF0000'
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${env.BUILD_URL})"
+  def summary = "${subject} (${env.BUILD_URL}) "Built commit: " + gitCurrCommitLog() + "\n""
   def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
@@ -25,5 +25,5 @@ def call(String buildStatus = 'STARTED') {
   }
 
   // Send notifications
-  slackSend (color: colorCode, message: details)
+  slackSend (color: colorCode, message: summary)
 }
