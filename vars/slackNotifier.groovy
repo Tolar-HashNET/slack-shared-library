0fa7ff13483244b16c42eec0a5ca7bbc1d16041b
abhisheks-cuelogic <abhishek.soni@cuelogic.com>
2018-07-20 19:20:08 +0530

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -19,7 +19,8 @@ def call(String buildStatus = 'STARTED') {
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}] (<${env.RUN_DISPLAY_URL}|Open>) (<${env.RUN_CHANGES_DISPLAY_URL}|  Changes>)'"
   def title = "${env.JOB_NAME} Build: ${env.BUILD_NUMBER}"
   def title_link = "${env.RUN_DISPLAY_URL}"
-  def branchName = "${env.BRANCH_NAME}"
+  def branchName = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
+  //def branchName = "${env.BRANCH_NAME}"
 
   def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
   def author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an'").trim()
