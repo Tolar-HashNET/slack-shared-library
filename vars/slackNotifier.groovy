a88ab8009a6425b1b484fa4800d086a628741c78
abhisheks-cuelogic <abhishek.soni@cuelogic.com>
2018-07-20 19:11:58 +0530

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -101,5 +101,5 @@ def call(String buildStatus = 'STARTED') {
   println attachments.toString()
 
   // Send notifications
-  slackSend (color: colorCode, message: subject, attachments: attachments.toString(), channel: channel)
+  slackSend (color: colorCode, message: subject, attachments: attachments.toString())
 }
