4360304f24e2b868a4128355de238e54b2cb1b35
abhisheks-cuelogic <abhishek.soni@cuelogic.com>
2018-07-20 19:31:02 +0530

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -12,6 +12,7 @@ def call(String buildStatus = 'STARTED') {
 
   // buildStatus of null means successfull
   buildStatus = buildStatus ?: 'SUCCESSFUL'
+  env.CHANGE_BRANCH ?: env.GIT_BRANCH ?: scm.branches[0]?.name?.split('/')[1] ?: 'UNKNOWN'
 
   // Default values
   def colorName = 'RED'
@@ -19,7 +20,8 @@ def call(String buildStatus = 'STARTED') {
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}] (<${env.RUN_DISPLAY_URL}|Open>) (<${env.RUN_CHANGES_DISPLAY_URL}|  Changes>)'"
   def title = "${env.JOB_NAME} Build: ${env.BUILD_NUMBER}"
   def title_link = "${env.RUN_DISPLAY_URL}"
-  def branchName = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
+  def branchName = env.CHANGE_BRANCH ?: env.GIT_BRANCH ?: scm.branches[0]?.name?.split('/')[1] ?: 'UNKNOWN'
+  //def branchName = sh(returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD').trim()
   //def branchName = "${env.BRANCH_NAME}"
 
   def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
