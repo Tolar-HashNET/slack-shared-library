34b635d2b401254c1016b359d17eacf6fa1dfc39
abhisheks-cuelogic <abhishek.soni@cuelogic.com>
2018-07-20 13:29:20 +0530

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -5,15 +5,13 @@
  */
 def call(String buildStatus = 'STARTED') {
   // build status of null means successful
-  buildStatus =  buildStatus ?: 'SUCCESSFUL'
+  buildStatus = buildStatus ?: 'SUCCESS'
 
   // Default values
-  def color = 'RED'
+  def colorName = 'RED'
   def colorCode = '#FF0000'
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
-  def author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${Commit}").trim()
-  def summary = "${subject} (${env.BUILD_URL}) ${author}\n"
+  def summary = "${subject} (${env.BUILD_URL})"
   def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
@@ -21,14 +19,13 @@ def call(String buildStatus = 'STARTED') {
   if (buildStatus == 'STARTED') {
     color = 'YELLOW'
     colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESSFUL') {
+  } else if (buildStatus == 'SUCCESS') {
     color = 'GREEN'
     colorCode = '#00FF00'
   } else {
     color = 'RED'
     colorCode = '#FF0000'
   }
-  
 
   // Send notifications
   slackSend (color: colorCode, message: summary)
