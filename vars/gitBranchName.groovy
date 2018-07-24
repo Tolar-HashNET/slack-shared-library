4f3ca787fe46eefb9493a8ec958d5bb018cc1d7b
abhisheks-cuelogic <abhishek.soni@cuelogic.com>
2018-07-24 14:53:00 +0530

    Update gitBranchName.groovy

--- vars/gitBranchName.groovy
+++ vars/gitBranchName.groovy
@@ -1,13 +1,5 @@
-#!/usr/bin/env groovy
-
-def call(Boolean safe = false) {
-  String branch = "${env.BRANCH_NAME}"
-
-  if(safe)
-  {
-    branch = branch.replaceAll("/", "-")
-    branch = branch.replaceAll("\\\\", "-")
-  }
-
-  return branch
-}
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
